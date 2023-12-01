185b1d914c9ca082ce02901fd6a3f3c32dea8d68
Pramuditha <pramudithamendis444@gmail.com>
2023-12-01 12:07:11 +0530

    Removed the Hi souts

--- src/main/java/p6/P6.java
+++ src/main/java/p6/P6.java
@@ -18,8 +18,7 @@ public class P6 {
 
 			String url = "jdbc:mysql://localhost:3306/jdbc";
 			Connection con = DriverManager.getConnection(url, "root", "");
-			//
-			System.out.println("Hii");
+
 			
 			Statement st = con.createStatement();
 			st.executeUpdate("insert into table1 values(900)");
